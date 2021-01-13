aa65a5fc34fe4acba67bcac80f3a777fd555d67b
SSamiKocaaga <sadettinsami@gmail.com>
2021-01-13 20:25:38 +0300

    Python Assignment - 007/1 (if-Statement)

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+UserName = 'Sami'
+Password = '4sftd'
+Entered = input()
+if Entered == UserName:
+    print('Hello {}! The password is : {} '.format(Entered, Password))
+else:
+    print('Hello {}! See you later.'.format(Entered))
